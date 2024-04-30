2bc20ec859e5271a57a519a8b9742da0d1ce4f7e
samir9927 <samir9927@hotmail.com>
2024-04-30 18:22:41 -0500

    Update Program.cs

--- CursoEstructuraDeDatos/ConsoleApp1/Program.cs
+++ CursoEstructuraDeDatos/ConsoleApp1/Program.cs
@@ -1,10 +1,28 @@
-﻿// Programa que sume los n primeros numeros
+﻿using System;
 
-var n = 10;
-var suma = 0;
-
-for (var i = 1; i <= n; i++)
+class Program
 {
-    suma += i;
+    static void Main()
+    {
+        // Solicitar al usuario que ingrese los dos números
+        Console.WriteLine("Ingrese el primer número:");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el segundo número:");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        // Realizar las operaciones
+        double suma = num1 + num2;
+        double resta = num1 - num2;
+        double multiplicacion = num1 * num2;
+
+        // Asegurarse de que el segundo número no sea cero antes de dividir
+        double division = (num2 != 0) ? (num1 / num2) : 0;
+
+        // Mostrar los resultados
+        Console.WriteLine("Suma: " + suma);
+        Console.WriteLine("Resta: " + resta);
+        Console.WriteLine("Multiplicación: " + multiplicacion);
+        Console.WriteLine("División: " + division);
+    }
 }
-Console.WriteLine("El resultado es: " + suma.ToString());
